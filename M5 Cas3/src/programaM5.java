de4c2d45eaa02e5ea0db534547e9fd7eaad1c3a4
jordi <jordiruiz@iesmontsia.org>
2014-02-20 17:24:30 +0100

    4

--- M5 Cas3/src/programaM5.java
+++ M5 Cas3/src/programaM5.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class programaM5 {
 	
-	// Mòdul factorial per canviar 3
+	// Mòdul factorial per canviar 4
 	public static int factorial(int base) {
 		int res = 1;
         if (base == 0) {

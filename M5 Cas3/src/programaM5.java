087666cd6690c9ae50e4b851bb191fb8fee818d7
jordi <jordi@jordi-NC003AA-ABE-IQ522es>
2014-02-28 11:36:33 +0100

    Proves de jordi per crear la wiki

--- M5 Cas3/src/programaM5.java
+++ M5 Cas3/src/programaM5.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class programaM5 {
 	
-	// Mòdul factorial per canviar aa
+	// Mòdul factorial per canviar aa2a
 	public static int factorial(int base) {
 		int res = 1;
         if (base == 0) {

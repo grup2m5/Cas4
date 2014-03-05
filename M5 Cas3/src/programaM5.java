01db52e25014087dd6270a58d53590e41f437844
manelmartinez <manelmartinez@iesmontsia.org>
2014-03-05 14:36:53 +0100

    Refactoring moure modul potencia

--- M5 Cas3/src/programaM5.java
+++ M5 Cas3/src/programaM5.java
@@ -4,36 +4,6 @@ import java.io.InputStreamReader;
 
 public class programaM5 {
 	
-	// Mòdul potència aa
-	public static int calcularPotencia(int base, int exponent) {
-		int resultat = 1;
-		if(base > 0){
-			if(exponent > 0){
-				for(int i=0;i < exponent; i++){
-					resultat = resultat * base;
-				}
-			}
-			else{
-				resultat = 1;
-			}
-		}
-		else if(base < 0){
-			if(exponent > 0){
-				for(int i=0;i < exponent; i++){
-					resultat = resultat * base;
-				}
-			}
-			else{
-				resultat = 1;
-			}
-		}
-		else{
-				resultat = 0;
-		}
-		return resultat;
-	}
-	
-	
 	// Mòdul principal 2 vegades
 	public static void main (String Args[]) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -70,7 +40,7 @@ public class programaM5 {
 				if (!correcte || (op1 == 0 && op2 == 0)){
 					System.out.println(error);
 				} else {
-					System.out.println(calcularPotencia(op1,op2));
+					System.out.println(programaM5potencia.calcularPotencia(op1,op2));
 				}
 			}
 		} else if (operacio.indexOf("!") >= 0) {
--- M5 Cas3/src/programaM5potencia.java
+++ M5 Cas3/src/programaM5potencia.java
@@ -1,4 +1,33 @@
 
 public class programaM5potencia {
 
+	// Mòdul potència aa
+	public static int calcularPotencia(int base, int exponent) {
+		int resultat = 1;
+		if(base > 0){
+			if(exponent > 0){
+				for(int i=0;i < exponent; i++){
+					resultat = resultat * base;
+				}
+			}
+			else{
+				resultat = 1;
+			}
+		}
+		else if(base < 0){
+			if(exponent > 0){
+				for(int i=0;i < exponent; i++){
+					resultat = resultat * base;
+				}
+			}
+			else{
+				resultat = 1;
+			}
+		}
+		else{
+				resultat = 0;
+		}
+		return resultat;
+	}
+
 }
